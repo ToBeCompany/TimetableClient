afd8fdcc28ebcef0b26d79ec46560f1a91f35b86
Dru128 <andreu012a@mail.ru>
2021-11-01 14:33:53 +0700

    добавил БД

--- app/src/main/java/com/example/timetable/MainActivity.kt
+++ app/src/main/java/com/example/timetable/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.timetable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+class MainActivity : AppCompatActivity()
+{
+
+    val t by lazy { findViewById<TextView>(R.id.tt) }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val db = Firebase.firestore
+
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        db.collection("USer").add(user)
+            .addOnSuccessListener { documentReference ->
+                t.text = "success"
+            }
+            .addOnFailureListener { e ->
+                t.text =  e.message.toString()
+                Log.d("Error", e.message.toString())
+
+            }
     }
 }
