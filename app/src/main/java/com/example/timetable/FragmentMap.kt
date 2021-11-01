4a5d8fffa98e10bfc0bedad357d4d69a2606f7eb
Kaneki-228 <ytude7042@gmail.com>
2021-11-01 12:58:44 +0700

    mapfragment

--- app/src/main/java/com/example/timetable/FragmentMap.kt
+++ app/src/main/java/com/example/timetable/FragmentMap.kt
@@ -6,14 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
-class MainFragment : Fragment() {
+class FragmentMap : Fragment(){
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
+
+
+
+
+
+
 }
